692e5abb5ade45fb53594bae9560ea337024ac77
lalithu <lalithuriti@gmail.com>
2021-05-04 11:36:31 +0530

    Falcon 9 ISS Approach - Successfully Resolved ODES

--- Falcon9_Traj.py
+++ Falcon9_Traj.py
@@ -3,51 +3,6 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 
 '''
-def firstorder(y, t):
-    tau = 5.0
-    K = -2.0
-    u = 1.0
-
-    dydt = (-y + K * u) / tau
-
-    return dydt
-
-
-y = odeint(firstorder, 0, [0, 10])
-print(y)
-'''
-
-'''
-# This is the Diffrential Equation Model, like where it solves for y(t)
-
-
-def model(y, t):
-    k = 0.3
-    dydt = -k * y
-    return dydt
-
-
-# Initial Condition,
-# When we're defining y0, we're really defining the value
-# of the solution function y(t) at the first input
-# value/moment t that the model is being integrated upon to be solved
-y_of_0 = 5
-
-# the time interval
-t = np.linspace(0, 20, 20)
-print(t)
-
-y = odeint(model, y_of_0, t)
-print(y)
-
-plt.plot(t, y)
-plt.xlabel('time')
-plt.ylabel('y(t)')
-
-plt.show()
-
-'''
-'''
 Falcon 9 v1.2 Full Thrust
 
 Stage 1
@@ -69,6 +24,24 @@ Falcon9 v1.2 Stage 1 (7.75, 16.46)
 - ∆V Stage 1 =  7.75 km/s = 27,900 km/h = 17,336.256 m/h
 - Mass Ratio = 16.46
 
+Stage 2
+- Thrust: 934 kN
+- Specific Impulse: 348 s
+- Dry Mass: 3, 900 kg
+- Propellant Mass: 92, 670 kg
+
+- Initial Mass: 96, 570 kg
+- Final Mass: 3, 900 kg
+
+- ISP = Ve / g
+- Ve = ISP · g
+- Ve = 348 · 9.807 = 3, 412.836 m/s = 3.412836 km/s
+
+Falcon9 v1.2 Stage 2 (10.95, 24.76)
+
+- ∆V Stage 2 = 10.95 km/s = 39, 420 km/h = 24, 494.452 m/h
+- Mass Ratio = 24.76
+
 '''
 
 # Constants
@@ -81,16 +54,18 @@ g = (G * M_Earth) / ((R * 1000) ** 2)  # m s^-2
 # print(g)
 
 m_stage_1 = 422000  # kg
+m_s_1_propellant = 370000  # kg
 m_dot_s_1 = 2312.5  # kg/s
 ve_s_1 = 2943  # m/s
-t_burn_s_1 = 160
+t_burn_s_1 = 162  # s
 
 m_stage_2 = 128000  # kg
+m_s_2_propellant = 108000  # kg
 m_dot_s_2 = 270  # kg/s
 ve_s_2 = 3433.5  # m/s
-t_burn_s_2 = 400
+t_burn_s_2 = 397  # s
 
-ISS_approach = 1000
+ISS_approach = 1000  # s
 
 
 def stage_1(state_var_launch, t):
@@ -102,7 +77,7 @@ def stage_1(state_var_launch, t):
     m = t * m_dot_s_1
 
     thrust_acc = thrust / (M - m) / 1000
-    g = - (G * M_Earth) / ((h * 1000) ** 2) / 1000
+    g = - (G * M_Earth) / ((h * 1000) ** 2) / 1000 * np.sin(90)
 
     dvdt = thrust_acc + g  # - (.3 * 0.25 * 200) / (M - m)
 
@@ -170,39 +145,32 @@ print(h_leo_traj)
 
 plt.figure()
 
-plt.subplot(2, 1, 1)
+plt.subplot(3, 1, 1)
 plt.plot(t_s_1, h_stage_1 - R)
 plt.plot(t_s_2, h_stage_2 - R)
 plt.plot(t_leo_traj, h_leo_traj - R)
 plt.xlabel('time')
 plt.ylabel('h(t)')
 
-plt.subplot(2, 1, 2)
+plt.subplot(3, 1, 2)
 plt.plot(t_s_1, v_stage_1 * 3600)
 plt.plot(t_s_2, v_stage_2 * 3600)
 plt.plot(t_leo_traj, v_leo_traj * 3600)
 plt.xlabel('time')
 plt.ylabel('v(t)')
 
+m_t_stage_1 = 52000 + (m_s_1_propellant - t_s_1 * m_dot_s_1)
+
+plt.subplot(3, 1, 3)
+plt.plot(t_s_1, m_t_stage_1)
+
+print(m_t_stage_1[-1])
+
+m_t_stage_2 = 20000 + m_s_2_propellant - (t_s_2 - 160) * m_dot_s_2
+
+plt.plot(t_s_2, m_t_stage_2)
+plt.plot(t_leo_traj, 20000 + m_t_stage_2[-1] * t_leo_traj * 0)
+plt.xlabel('time')
+plt.ylabel('m(t)')
 
 plt.show()
-
-'''
-Stage 2
-- Thrust: 934 kN
-- Specific Impulse: 348 s
-- Dry Mass: 3, 900 kg
-- Propellant Mass: 92, 670 kg
-
-- Initial Mass: 96, 570 kg
-- Final Mass: 3, 900 kg
-
-- ISP = Ve / g
-- Ve = ISP · g
-- Ve = 348 · 9.807 = 3, 412.836 m/s = 3.412836 km/s
-
-Falcon9 v1.2 Stage 2 (10.95, 24.76)
-
-- ∆V Stage 2 = 10.95 km/s = 39, 420 km/h = 24, 494.452 m/h
-- Mass Ratio = 24.76
-'''
